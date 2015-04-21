1a3879da4dcd42b8b834a1c8fb1b781efb547b9c
Carlos Torres <carlos.torres@openebusiness.pe>
2015-04-21 14:44:46 -0500

    Mi primer commit

---
+++ src/main/webapp/admin/scripts/services/BookingFactory.js
@@ -0,0 +1,4 @@
+angular.module('ticketMonster').factory('BookingResource', function($resource){
+    var resource = $resource('../rest/forge/bookings/:BookingId',{BookingId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
+    return resource;
+});
---
+++ src/main/webapp/admin/scripts/services/VenueFactory.js
@@ -0,0 +1,4 @@
+angular.module('ticketMonster').factory('VenueResource', function($resource){
+    var resource = $resource('../rest/forge/venues/:VenueId',{VenueId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
+    return resource;
+});
